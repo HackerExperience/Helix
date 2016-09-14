3ff64d06503b805d8d86bd8e409d01bc0cff79c5
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-14 19:41:44 -0300

    Removed pry usage from Auth jwt.

--- apps/auth/lib/auth/jwt.ex
+++ apps/auth/lib/auth/jwt.ex
@@ -1,5 +1,3 @@
-require IEx
-
 defmodule HELM.Auth.JWT do
 
   alias HELF.Error
@@ -8,13 +6,9 @@ defmodule HELM.Auth.JWT do
     value = Guardian.encode_and_sign(%{account_id: id}, :access)
     case value do
       {:ok, jwt, _full_claims} ->
-        IEx.pry
         {:reply, {:ok, %{"token" => jwt}}}
       {:error, err} ->
-        IEx.pry
         {:reply, {:error, Error.format_reply(:internal, err)}}
-      _ ->
-        IEx.pry
     end
   end
 
