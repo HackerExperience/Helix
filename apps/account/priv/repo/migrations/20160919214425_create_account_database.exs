7e325852a11629ae9784662db825338548fc7752
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-19 18:55:43 -0300

    Added Account initial migration.

---
+++ apps/account/priv/repo/migrations/20160919214425_create_account_database.exs
@@ -0,0 +1,17 @@
+defmodule HELM.Account.Repo.Migrations.CreateAccountDatabase do
+  use Ecto.Migration
+
+  def change do
+    create table(:accounts, primary_key: false) do
+      add :account_id, :string, primary_key: true
+      add :password, :string, null: false
+      add :email, :string
+      add :confirmed, :boolean, default: false
+
+      timestamps
+    end
+
+    create unique_index(:accounts, [:account_id], name: :unique_account_id)
+    create unique_index(:accounts, [:email], name: :unique_account_email)
+  end
+end
