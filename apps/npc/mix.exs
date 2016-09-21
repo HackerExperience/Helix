0dfa0da3b9caf4deb55d03eb7300888052299ad5
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-21 19:26:30 -0300

    Adding tests to NPC Service.

--- apps/npc/mix.exs
+++ apps/npc/mix.exs
@@ -37,6 +37,7 @@ defmodule HELM.NPC.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:framework, in_umbrella: true},
+     {:hell, in_umbrella: true},
      {:postgrex, ">= 0.0.0"},
      {:ecto, "~> 2.0"}]
   end
--- apps/npc/test/npc_test.exs
+++ apps/npc/test/npc_test.exs
@@ -1,8 +1,28 @@
 defmodule HELM.NPCTest do
   use ExUnit.Case
-  doctest HELM.NPC
+
+  alias HELF.Broker
+  alias HELM.NPC
 
   test "the truth" do
     assert 1 + 1 == 2
+  end
+
+  test "npc creation" do
+    {:ok, _} = NPC.Controller.new_npc(%{})
+  end
+
+  test "npc creation using the broker" do
+    {:ok, _} = Broker.call("npc:create", %{})
+  end
+
+  test "npc removal" do
+    {:ok, npc} = NPC.Controller.new_npc(%{})
+    {:ok, _} = NPC.Controller.remove_npc(npc.npc_id)
+  end
+
+  test "npc removal using the broker" do
+    {:ok, npc} = Broker.call("npc:create", %{})
+    {:ok, _} = Broker.call("npc:remove", %{npc_id: npc.npc_id})
   end
 end
