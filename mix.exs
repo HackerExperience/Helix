33f3542a3a0e4d3bf25d25ce44e7326d21e0ac30
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-22 17:26:31 -0300

    Fixing wrong namespace on umbrella mixfile, also removing useless boilerplate.

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule Helm.Mixfile do
+defmodule HELM.Mixfile do
   use Mix.Project
 
   def project do
@@ -8,18 +8,6 @@ defmodule Helm.Mixfile do
      deps: deps]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options.
-  #
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps folder
   defp deps do
     []
   end
