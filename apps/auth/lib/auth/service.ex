34bc3bad4f250db91b5028ea7b08ddab990d6b1e
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-09-23 17:19:06 -0300

    Fixing Broker usage at Auth.Service.

--- apps/auth/lib/auth/service.ex
+++ apps/auth/lib/auth/service.ex
@@ -10,15 +10,25 @@ defmodule HELM.Auth.Service do
 
   def init(_args) do
     Broker.subscribe(:auth, "auth:create", call:
-      fn _,_,id,_ ->
-        Auth.JWT.generate(id)
+      fn pid,_,id,timeout ->
+        GenServer.call(pid, {:auth_create, id}, timeout)
       end)
 
     Broker.subscribe(:auth, "auth:verify", call:
-      fn _,_,jwt,_ ->
-          Auth.JWT.verify(jwt)
+      fn pid,_,jwt,timeout ->
+        GenServer.call(pid, {:auth_verify, id}, timeout)
       end)
 
     {:ok, %{}}
+  end
+
+  def handle_call({:auth_create, id}, _from, state) do
+    result = Auth.JWT.generate(id)
+    {:reply, result, state}
+  end
+
+  def handle_call({:auth_verify, jwt}, _from, state) do
+    result = Auth.JWT.verify(jwt)
+    {:reply, result, state}
   end
 end
