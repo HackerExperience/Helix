21fffb847bc4c4308650129ade22da52a06c96e8
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-10-20 17:37:18 -0200

    Updating UUID docs and also part of the API, tests are passing.

--- apps/hell/lib/hell/uuid.ex
+++ apps/hell/lib/hell/uuid.ex
@@ -14,46 +14,50 @@ defmodule HELL.UUID do
 
   @doc """
   Generates UUID with given `domain`, `meta1` and `meta2`.
-  Domain is must always use two characters, metas must use just a single.
+  Domain must always use two characters, metas must use just a single.
   The default value for metas is `0`.
   """
   def create(domain, opts \\ []) do
-    head = header(domain, opts)
-    if head, do: merge_header(head), else: nil
+    with {:ok, header} <- header(domain, opts) do
+      {:ok, merge(UUID.uuid4(), header)}
+    end
   end
 
   @doc """
+  Same as `create/2` but raises `ArgumentError` when header not valid.
+  """
+  def create!(domain, opts \\ []) do
+    case create(domain, opts) do
+      {:ok, id} -> id
+      :error -> raise ArgumentError, "invalid uuid header"
+    end
+  end
+
+  @doc """
+  Parses the header, never use this with game logic, this is only for debug code or `iex` usage.
+  """
+  def debug(header) do
+    <<domain::bytes-size(2), meta1::bytes-size(1), meta2::bytes-size(1), rest::binary>> = header
+    %{domain: domain, meta1: meta1, meta2: meta2, uuid_part: rest}
+  end
+
+  @docp """
   Generates a valid header with given `domain`, `meta1` and `meta2`.
   Follow the same format rules from `create/2`.
   Useful for caching headers on module attributes, merge the header into an UUID later using
   `merge_header/1`.
   """
-  def header(domain, opts \\ []) do
+  defp header(domain, opts) do
     meta1 = Keyword.get(opts, :meta1, "0")
     meta2 = Keyword.get(opts, :meta2, "0")
 
     with true <- valid_domain?(domain),
          true <- valid_meta?(meta1),
          true <- valid_meta?(meta2) do
-      domain <> meta1 <> meta2
+      {:ok, domain <> meta1 <> meta2}
     else
-      _ -> nil
+      _ -> :error
     end
-  end
-
-  @doc """
-  Merges a header into a new UUID, this won't validate the header so always use a header
-  generated with `header/2`.
-  """
-  def merge_header(head),
-    do: if head, do: merge(UUID.uuid4(), head), else: nil
-
-  @doc """
-  Parses the header, never use this to on game logic, this is only for debug code or `iex` usage.
-  """
-  def parse(header) do
-    <<domain::bytes-size(2), meta1::bytes-size(1), meta2::bytes-size(1), rest::binary>> = header
-    %{domain: domain, meta1: meta1, meta2: meta2, uuid: rest}
   end
 
   @docp """
@@ -70,6 +74,7 @@ defmodule HELL.UUID do
 
   @docp """
   Validates meta attributes.
+  Meta attributes must be a string composed of a single hexadecimal character.
   """
   for x <- @hex_chars do
     defp valid_meta?("#{unquote(x)}"),
@@ -81,6 +86,7 @@ defmodule HELL.UUID do
 
   @docp """
   Validates domain name.
+  Domain name must be a string composed of two hexadecimal characters.
   """
   for x <- @hex_chars, y <- @hex_chars do
     defp valid_domain?("#{unquote(x)}#{unquote(y)}"),
--- apps/hell/test/uuid_test.exs
+++ apps/hell/test/uuid_test.exs
@@ -5,54 +5,39 @@ defmodule HELL.UUIDTest do
 
   describe "create/2" do
     test "valid format" do
-      assert "ff00" <> _ = HUUID.create("ff")
-      assert "ffa0" <> _ = HUUID.create("ff", meta1: "a")
-      assert "ff0b" <> _ = HUUID.create("ff", meta2: "b")
-      assert "ffab" <> _ = HUUID.create("ff", meta1: "a", meta2: "b")
+      assert {:ok, "ff00" <> _} = HUUID.create("ff")
+      assert {:ok, "ffa0" <> _} = HUUID.create("ff", meta1: "a")
+      assert {:ok, "ff0b" <> _} = HUUID.create("ff", meta2: "b")
+      assert {:ok, "ffab" <> _} = HUUID.create("ff", meta1: "a", meta2: "b")
     end
 
     test "invalid format" do
-      assert nil == HUUID.create("ue")
-      assert nil == HUUID.create("ff", meta1: "u")
-      assert nil == HUUID.create("ff", meta2: "w")
-      assert nil == HUUID.create("ff", meta1: "u", meta2: "e")
+      assert :error == HUUID.create("ue")
+      assert :error == HUUID.create("ff", meta1: "u")
+      assert :error == HUUID.create("ff", meta2: "w")
+      assert :error == HUUID.create("ff", meta1: "u", meta2: "e")
     end
   end
 
-  describe "header/2" do
-    test "correct format" do
-      assert "ff00" = HUUID.header("ff")
-      assert "ffa0" = HUUID.header("ff", meta1: "a")
-      assert "ff0b" = HUUID.header("ff", meta2: "b")
-      assert "ffab" = HUUID.header("ff", meta1: "a", meta2: "b")
+  describe "create!/2" do
+    test "success" do
+      assert "ff00" <> _ = HUUID.create!("ff")
     end
 
-    test "invalid format" do
-      assert nil == HUUID.header("ue")
-      assert nil == HUUID.header("ff", meta1: "u")
-      assert nil == HUUID.header("ff", meta2: "w")
-      assert nil == HUUID.header("ff", meta1: "u", meta2: "e")
+    test "failure" do
+      assert_raise ArgumentError, fn ->
+        HUUID.create!("ue")
+      end
     end
   end
 
-  describe "merge_header/2" do
-    test "prebuild header" do
-      header = HUUID.header("ff", meta1: "a", meta2: "b")
-      assert "ffab" <> _ = HUUID.merge_header(header)
-    end
-
-    test "handmade header" do
-      header = "abcdef"
-      assert "abcdef" <> _ = HUUID.merge_header(header)
-    end
-
-    test "empty header" do
-      assert is_binary HUUID.merge_header("")
-    end
-
-    test "invalid header" do
-      header = HUUID.header("ue")
-      assert nil == HUUID.merge_header(header)
+  describe "debug/1" do
+    test "parsing debug information" do
+      a = "ff"
+      b = "a"
+      c = "b"
+      assert {:ok, uuid} = HUUID.create(a, meta1: b, meta2: c)
+      assert %{domain: domain, meta1: a, meta2: b} = HUUID.debug(uuid)
     end
   end
 end
