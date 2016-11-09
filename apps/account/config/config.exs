5237c1fe72037073c4a3dbff25af45869cd49979
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-11-09 15:24:13 -0200

    * Use HELIX_JWT_KEY environment variable. * Change AccountService to not crash when "account:login" is called without email and password. * Change Account.login/2 to not crash when called with nil params.

--- apps/account/config/config.exs
+++ apps/account/config/config.exs
@@ -15,16 +15,10 @@ config :account, HELM.Account.Repo,
   ]
 
 config :guardian, Guardian,
-  issuer: "Ws",
-  ttl: { 2, :days},
-  allowed_algos: ["ES512"],
-  secret_key: %{
-    "crv" => "P-521",
-    "d" => "axDuTtGavPjnhlfnYAwkHa4qyfz2fdseppXEzmKpQyY0xd3bGpYLEF4ognDpRJm5IRaM31Id2NfEtDFw4iTbDSE",
-    "kty" => "EC",
-    "x" => "AL0H8OvP5NuboUoj8Pb3zpBcDyEJN907wMxrCy7H2062i3IRPF5NQ546jIJU3uQX5KN2QB_Cq6R_SUqyVZSNpIfC",
-    "y" => "ALdxLuo6oKLoQ-xLSkShv_TA0di97I9V92sg1MKFava5hKGST1EKiVQnZMrN3HO8LtLT78SNTgwJSQHAXIUaA-lV"
-  },
+  issuer: "account",
+  ttl: {1, :days},
+  allowed_algos: ["HS512"],
+  secret_key: System.get_env("HELIX_JWK_KEY"),
   serializer: HELM.Account.Model.Session
 
 import_config "#{Mix.env}.exs"
