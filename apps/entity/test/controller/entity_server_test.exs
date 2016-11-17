a0192cbc3b5e9d761f248a18cfbf70191f29adf2
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-11-17 21:19:43 -0200

    Fix EntityServer.find/1 test contracts.

--- apps/entity/test/controller/entity_server_test.exs
+++ apps/entity/test/controller/entity_server_test.exs
@@ -21,23 +21,24 @@ defmodule HELM.Entity.Controller.EntityServerTest do
     assert {:ok, _} = CtrlEntityServer.create(entity.entity_id, id)
   end
 
-  describe "find/2" do
-    test "success", %{type: type, payload: payload, id: id} do
+  describe "find/1" do
+    test "found servers", %{type: type, payload: payload, id: id} do
       {:ok, _} = CtrlEntityType.create(type)
       {:ok, entity} = CtrlEntity.create(payload)
-      {:ok, entity_server} = CtrlEntityServer.create(entity.entity_id, id)
-      assert {:ok, ^entity_server} = CtrlEntityServer.find(entity.entity_id, id)
+      {:ok, entity_server_a} = CtrlEntityServer.create(entity.entity_id, id)
+      {:ok, entity_server_b} = CtrlEntityServer.create(entity.entity_id, IPv6.generate([]))
+      assert [^entity_server_a | [^entity_server_b | _]] = CtrlEntityServer.find(entity.entity_id)
     end
 
-    test "failure" do
-      assert {:error, :notfound} = CtrlEntityServer.find(IPv6.generate([]), IPv6.generate([]))
+    test "no servers found" do
+      assert [] = CtrlEntityServer.find(IPv6.generate([]))
     end
   end
 
   test "delete/1 idempotency", %{type: type, payload: payload, id: id} do
     {:ok, _} = CtrlEntityType.create(type)
     {:ok, entity} = CtrlEntity.create(payload)
-    {:ok, entity_server} = CtrlEntityServer.create(entity.entity_id, id)
+    {:ok, _} = CtrlEntityServer.create(entity.entity_id, id)
     assert :ok = CtrlEntityServer.delete(entity.entity_id, id)
     assert :ok = CtrlEntityServer.delete(entity.entity_id, id)
   end
