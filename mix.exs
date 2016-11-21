6fc420c4f70e6175f553a7b5f134cb097b320f83
mememori <charlotte.elixir@gmail.com>
2016-11-21 18:41:55 -0200

    Force database to be dropped on each test to avoid collision

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule HELM.Mixfile do
       apps_path: "apps",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
+      aliases: aliases,
       deps: deps]
   end
 
@@ -15,5 +16,10 @@ defmodule HELM.Mixfile do
       {:ex_doc, "~> 0.14", only: :dev},
       {:credo, "~> 0.5", only: [:dev, :test]},
       {:dialyze, "~> 0.2", only: [:dev, :test]}]
+  end
+
+  defp aliases do
+    [
+      test: ["ecto.drop", "ecto.create --quiet", "ecto.migrate --quiet", "test"]]
   end
 end
