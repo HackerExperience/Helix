739a0603358539dd554b609630579c54d2c9ad33
mememori <charlotte.elixir@gmail.com>
2016-12-02 23:19:09 -0200

    Use a surrogate key on module_roles to make everything easier

---
+++ apps/software/priv/repo/migrations/20161203005239_fix_module_role_pk.exs
@@ -0,0 +1,26 @@
+defmodule HELM.Software.Repo.Migrations.FixModuleRolePK do
+  use Ecto.Migration
+
+  def change do
+    alter table(:modules) do
+      remove :module_role
+      remove :file_id
+    end
+
+    alter table(:module_roles) do
+      remove :file_type
+      remove :module_role
+      add :module_role_id, :inet, primary_key: true
+      add :module_role, :string
+      add :file_type, references(:file_types, column: :file_type, type: :string), null: false
+
+    end
+
+    alter table(:modules) do
+      add :file_id, references(:files, column: :file_id, type: :inet), primary_key: true
+      add :module_role_id, references(:module_roles, column: :module_role_id, type: :inet), primary_key: true
+    end
+
+    create unique_index(:module_roles, [:file_type, :module_role], name: :file_type_module_role_unique_constraint)
+  end
+end
