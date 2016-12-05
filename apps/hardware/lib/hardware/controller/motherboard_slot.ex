842559a4477b149ccf89eebc6f85c92b9d0ee236
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-12-05 19:22:14 -0200

    Remove Motherboard's link and unlink idempotency. Fix invalid typespecs.

--- apps/hardware/lib/hardware/controller/motherboard_slot.ex
+++ apps/hardware/lib/hardware/controller/motherboard_slot.ex
@@ -37,16 +37,35 @@ defmodule HELM.Hardware.Controller.MotherboardSlot do
     end
   end
 
-  @spec link(slot :: HELL.PK.t, component :: HELL.PK.t | nil) :: {:ok, MdlMoboSlot.t} | {:error, :notfound | Ecto.Changeset.t}
+  @spec link(slot_id :: HELL.PK.t, component :: HELL.PK.t | nil) ::
+    {:ok, MdlMoboSlot.t}
+    | {:error, :linked_component | :linked_slot | :notfound | Ecto.Changeset.t}
   def link(slot_id, link_component_id) do
-    params = %{link_component_id: link_component_id}
-    update(slot_id, params)
+    with \
+      :unused_component <- unused_component?(link_component_id),
+      :empty_slot <- empty_slot?(slot_id)
+    do
+      update(slot_id, %{link_component_id: link_component_id})
+    else
+      {:error, msg} ->
+        {:error, msg}
+      msg ->
+        {:error, msg}
+    end
   end
 
-  @spec unlink(HELL.PK.t) :: {:ok, MdlMoboSlot.t} | {:error, Ecto.Changeset.t} | {:error, :notfound | Ecto.Changeset.t}
+  @spec unlink(HELL.PK.t) ::
+    {:ok, MdlMoboSlot.t}
+    | {:error, :empty_slot | :notfound | Ecto.Changeset.t}
   def unlink(slot_id) do
-    params = %{link_component_id: nil}
-    update(slot_id, params)
+    case empty_slot?(slot_id) do
+      :linked_slot ->
+        update(slot_id, %{link_component_id: nil})
+      :empty_slot ->
+        {:error, :empty_slot}
+      error ->
+        error
+    end
   end
 
   @spec delete(HELL.PK.t) :: no_return
@@ -68,12 +87,40 @@ defmodule HELM.Hardware.Controller.MotherboardSlot do
   end
 
   @spec parse_motherboard_spec(%{String.t => any}) ::
-    [%{slot_internal_id: non_neg_integer, link_component_type: String.t}]
+  [%{slot_internal_id: non_neg_integer, link_component_type: String.t}]
   def parse_motherboard_spec(component_spec) do
     Enum.map(component_spec.spec["slots"], fn {id, spec} ->
       %{
         slot_internal_id: id,
         link_component_type: spec["type"]}
     end)
+  end
+
+  @spec unused_component?(HELL.PK.t) :: :unused_component | :linked_component
+  defp unused_component?(component_id) do
+    MdlMoboSlot
+    |> where([s], s.link_component_id == ^component_id)
+    |> Repo.all()
+    |> Enum.empty?()
+    |> case do
+      true ->
+        :unused_component
+      false ->
+        :linked_component
+    end
+  end
+
+  @spec empty_slot?(HELL.PK.t) :: :empty_slot | :linked_slot | {:error, :notfound}
+  defp empty_slot?(slot_id) do
+    case find(slot_id) do
+      {:ok, slot} ->
+        if slot.link_component_id == nil do
+          :empty_slot
+        else
+          :linked_slot
+        end
+      error ->
+        error
+    end
   end
 end
