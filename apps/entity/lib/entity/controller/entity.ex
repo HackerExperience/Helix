7bef01726264352928d1723366ba86e666043374
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2016-12-15 21:44:31 -0200

    Add Entity's specialized table for Accounts.

--- apps/entity/lib/entity/controller/entity.ex
+++ apps/entity/lib/entity/controller/entity.ex
@@ -1,14 +1,18 @@
 defmodule HELM.Entity.Controller.Entity do
 
   alias HELM.Entity.Model.Entity, as: MdlEntity
+  alias Helix.Entity.Model.EntityAccount
   alias HELM.Entity.Repo
   import Ecto.Query, only: [where: 3]
 
-  @spec create(MdlEntity.creation_params) :: {:ok, MdlEntity.t} | {:error, Ecto.Changeset.t}
+  @spec create(MdlEntity.creation_params) :: {:ok, MdlEntity.t} | no_return
   def create(params) do
-    params
-    |> MdlEntity.create_changeset()
-    |> Repo.insert()
+    Repo.transaction(fn ->
+      params
+      |> MdlEntity.create_changeset()
+      |> Repo.insert!()
+      |> create_specialization()
+    end)
   end
 
   @spec find(MdlEntity.id) :: {:ok, MdlEntity.t} | {:error, :notfound}
@@ -28,5 +32,22 @@ defmodule HELM.Entity.Controller.Entity do
     |> Repo.delete_all()
 
     :ok
+  end
+
+  @spec create_specialization(MdlEntity.t) :: MdlEntity.t
+  defp create_specialization(entity = %{entity_type: "account"}) do
+    %{entity_id: entity.entity_id}
+    |> EntityAccount.create_changeset()
+    |> Repo.insert!()
+
+    entity
+  end
+  defp create_specialization(entity = %{entity_type: "npc"}) do
+    # FIXME
+    entity
+  end
+  defp create_specialization(entity = %{entity_type: "clan"}) do
+    # FIXME
+    entity
   end
 end
---
+++ apps/entity/lib/entity/model/entity_account.ex
@@ -0,0 +1,32 @@
+defmodule Helix.Entity.Model.EntityAccount do
+
+  use Ecto.Schema
+
+  alias HELM.Entity.Model.Entity, as: Entity, warn: false
+  import Ecto.Changeset
+
+  @type t :: %__MODULE__{
+    entity_id: Entity.id,
+    entity: Entity
+  }
+
+  @type creation_params :: %{entity_id: MdlEntity.id}
+
+  @creation_fields ~w/entity_id/a
+
+  @primary_key false
+  schema "entity_accounts" do
+    belongs_to :entity, Entity,
+      foreign_key: :entity_id,
+      references: :entity_id,
+      type: EctoNetwork.INET,
+      primary_key: true
+  end
+
+  @spec create_changeset(creation_params) :: Ecto.Changeset.t
+  def create_changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @creation_fields)
+    |> validate_required([:entity_id])
+  end
+end
