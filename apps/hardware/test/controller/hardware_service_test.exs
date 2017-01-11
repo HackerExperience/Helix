778e6b20f4244de64283f90e7c734a1fbaf58575
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2017-01-11 21:48:19 -0200

    - Move HardwareService's test fixture to it's own function. - Change the fixture insertion method so checks wouldn't be needed.

--- apps/hardware/test/controller/hardware_service_test.exs
+++ apps/hardware/test/controller/hardware_service_test.exs
@@ -4,83 +4,83 @@ defmodule Helix.Hardware.Controller.HardwareServiceTest do
 
   alias HELF.Broker
   alias Helix.Hardware.Repo
-  alias Helix.Hardware.Controller.ComponentSpec, as: ComponentSpecController
   alias Helix.Hardware.Controller.Motherboard, as: MotherboardController
   alias Helix.Hardware.Model.ComponentSpec
   alias Helix.Hardware.Model.MotherboardSlot
 
   @moduletag :umbrella
 
-  setup_all do
-    if is_nil Repo.get_by(ComponentSpec, spec_id: "MOBO01") do
-      Repo.transaction fn ->
-        mobo_params = %{
-          component_type: "mobo",
-          spec: %{
-            spec_code: "MOBO01",
-            spec_type: "mobo",
-            slots: %{
-              "0" => %{
-                type: "cpu"
-              },
-              "1" => %{
-                type: "ram"
-              },
-              "2" => %{
-                type: "hdd"
-              },
-              "3" => %{
-                type: "nic"
-              }
+  setup_all [:component_fixtures]
+
+  defp component_fixtures(_context) do
+    Repo.transaction fn ->
+      mobo_params = %{
+        component_type: "mobo",
+        spec: %{
+          spec_code: "MOBO01",
+          spec_type: "mobo",
+          slots: %{
+            "0" => %{
+              type: "cpu"
+            },
+            "1" => %{
+              type: "ram"
+            },
+            "2" => %{
+              type: "hdd"
+            },
+            "3" => %{
+              type: "nic"
             }
           }
         }
+      }
 
-        cpu_params = %{
-          component_type: "cpu",
-          spec: %{
-            spec_code: "CPU01",
-            spec_type: "cpu",
-          }
+      cpu_params = %{
+        component_type: "cpu",
+        spec: %{
+          spec_code: "CPU01",
+          spec_type: "cpu",
         }
+      }
 
-        ram_params = %{
-          component_type: "ram",
-          spec: %{
-            spec_code: "RAM01",
-            spec_type: "ram"
-          }
+      ram_params = %{
+        component_type: "ram",
+        spec: %{
+          spec_code: "RAM01",
+          spec_type: "ram"
         }
+      }
 
-        hdd_params = %{
-          component_type: "hdd",
-          spec: %{
-            spec_code: "HDD01",
-            spec_type: "hdd"
-          }
+      hdd_params = %{
+        component_type: "hdd",
+        spec: %{
+          spec_code: "HDD01",
+          spec_type: "hdd"
         }
+      }
 
-        nic_params = %{
-          component_type: "nic",
-          spec: %{
-            spec_code: "NIC01",
-            spec_type: "nic"
-          }
+      nic_params = %{
+        component_type: "nic",
+        spec: %{
+          spec_code: "NIC01",
+          spec_type: "nic"
         }
+      }
 
-        spec_params = [
-          mobo_params,
-          cpu_params,
-          ram_params,
-          hdd_params,
-          nic_params
-        ]
+      spec_params = [
+        mobo_params,
+        cpu_params,
+        ram_params,
+        hdd_params,
+        nic_params
+      ]
 
-        Enum.map(spec_params, fn params ->
-          {:ok, spec} = ComponentSpecController.create(params)
-          spec
-        end)
-      end
+      Enum.each(spec_params, fn params ->
+        params
+        |> ComponentSpec.create_changeset()
+        |> Repo.insert(on_conflict: :nothing)
+      end)
     end
 
     :ok
