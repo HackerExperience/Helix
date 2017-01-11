27c641136d017484e1bf04be33a8fdcb58f919f9
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2017-01-11 00:30:21 -0200

    Update HardwareService tests.

--- apps/hardware/test/controller/hardware_service_test.exs
+++ apps/hardware/test/controller/hardware_service_test.exs
@@ -1,6 +1,6 @@
 defmodule Helix.Hardware.Controller.HardwareServiceTest do
 
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
   alias HELF.Broker
   alias Helix.Hardware.Controller.Motherboard, as: MotherboardController
@@ -8,46 +8,44 @@ defmodule Helix.Hardware.Controller.HardwareServiceTest do
 
   @moduletag :umbrella
 
-  defp forward_event(event) do
-    ref = make_ref()
-    Broker.subscribe(event, cast: fn pid, _, data, _ ->
-      send pid, {ref, data}
-    end)
-    ref
-  end
-
   describe "after account creation" do
     setup do
+      ref = make_ref()
+
+      Broker.subscribe("event:motherboard:setup", cast: fn pid, _, data, _ ->
+        send pid, {ref, data}
+      end)
+
       email = Burette.Internet.email()
       password = Burette.Internet.password()
-      params = %{email: email, password_confirmation: password, password: password}
-      {_, {:ok, _}} = Broker.call("account:create", params)
+      params = %{
+        email: email,
+        password_confirmation: password,
+        password: password}
 
-      :ok
+      case Broker.call("account:create", params) do
+        {_, {:ok, _}} ->
+          {:ok, ref: ref}
+        _ ->
+          :error
+      end
     end
 
-    test "motherboard is created" do
-      ref_create = forward_event("event:motherboard:created")
-      ref_setup = forward_event("event:motherboard:setup")
-
-      assert_receive {^ref_create, motherboard_id}, 5_000
-      assert_receive {^ref_setup, {^motherboard_id, _}}, 5_000
-
+    test "motherboard is created",  %{ref: ref} do
+      assert_receive {^ref, %{motherboard_id: motherboard_id}}, 5_000
       assert {:ok, _} = MotherboardController.find(motherboard_id)
     end
 
-    test "motherboard slots are created" do
-      ref = forward_event("event:motherboard:setup")
-      assert_receive {^ref, {motherboard_id, _}}, 5_000
-      slots = MotherboardSlotController.find_by(motherboard_id: motherboard_id)
+    test "motherboard slots are created", %{ref: ref} do
+      assert_receive {^ref, event}, 5_000
+      slots = MotherboardSlotController.find_by(motherboard_id: event.motherboard_id)
 
       refute Enum.empty?(slots)
     end
 
-    test "motherboard slots are linked" do
-      ref = forward_event("event:motherboard:setup")
-      assert_receive {^ref, {motherboard_id, _}}, 5_000
-      slots = MotherboardSlotController.find_by(motherboard_id: motherboard_id)
+    test "motherboard slots are linked", %{ref: ref} do
+      assert_receive {^ref, event}, 5_000
+      slots = MotherboardSlotController.find_by(motherboard_id: event.motherboard_id)
 
       Enum.each(slots, fn slot ->
         assert slot.link_component_id
