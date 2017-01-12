6b20789e69e8e4e87a71d409304d971dbaecaf34
Christian Ferraz Lemos de Sousa <kress@hotmail.com.br>
2017-01-12 16:35:17 -0200

    - Make ServerService attach given motherboard into it's server after an "event:motherboard:setup" event. - Test that the motherboard is attached after creating an account. - Cast events "event:server:attached" and "event:server:detached" from ServerService.

--- apps/server/test/controller/server_service_test.exs
+++ apps/server/test/controller/server_service_test.exs
@@ -6,25 +6,42 @@ defmodule Helix.Server.Controller.ServerServiceTest do
 
   @moduletag :umbrella
 
-  setup do
-    email = Burette.Internet.email()
-    password = Burette.Internet.password()
-    params = %{email: email, password_confirmation: password, password: password}
-    {:ok, params: params}
+  defp forward_broker_cast(topic) do
+    ref = make_ref()
+
+    Broker.subscribe(topic, cast: fn pid, _, data, _ ->
+      send pid, {ref, data}
+    end)
+
+    ref
   end
 
-  describe "server creation" do
-    test "after account creation", %{params: params} do
-      ref = make_ref()
-      Broker.subscribe("event:server:created", cast: fn pid, _, data, _ ->
-        send pid, {ref, data}
-      end)
+  describe "after account creation" do
+    setup do
+      email = Burette.Internet.email()
+      password = Burette.Internet.password()
+      params = %{email: email, password_confirmation: password, password: password}
+      {:ok, params: params}
+    end
+
+    test "server is created", %{params: params} do
+      ref = forward_broker_cast("event:server:created")
 
       {_, {:ok, account}} =
         Broker.call("account:create", params)
       assert_receive {^ref, {server_id, entity_id}}
       assert account.account_id == entity_id
       assert {_, {:ok, _}} = Broker.call("server:query", server_id)
+    end
+
+    test "server attaches a motherboard", %{params: params} do
+      ref = forward_broker_cast("event:server:attached")
+
+      {_, {:ok, _}} =
+        Broker.call("account:create", params)
+      assert_receive {^ref, msg}
+      assert {_, {:ok, server}} = Broker.call("server:query", msg.server_id)
+      assert server.motherboard_id === msg.motherboard_id
     end
   end
 end
