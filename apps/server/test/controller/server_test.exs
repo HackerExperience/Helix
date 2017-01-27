5933e252fee150c8c241376e13f904f3a1140820
kress95 <kress@hotmail.com.br>
2017-01-27 20:32:15 -0200

    Refactor Server tests.

--- apps/server/test/controller/server_test.exs
+++ apps/server/test/controller/server_test.exs
@@ -2,70 +2,83 @@ defmodule Helix.Server.Controller.ServerTest do
 
   use ExUnit.Case, async: true
 
-  alias HELL.PK
-  alias HELL.TestHelper.Random, as: HRand
+  alias HELL.TestHelper.Random, as: Random
   alias Helix.Server.Controller.Server, as: ServerController
+  alias Helix.Server.Model.Server
   alias Helix.Server.Model.ServerType
   alias Helix.Server.Repo
 
-  @server_type HRand.string(min: 20)
-
   setup_all do
-    %{server_type: @server_type}
-    |> ServerType.create_changeset()
-    |> Repo.insert!()
+    server_types =
+      ServerType
+      |> Repo.all()
 
-    :ok
+    {:ok, server_types: server_types}
   end
 
-  setup do
-    payload = %{server_type: @server_type}
-    {:ok, payload: payload}
+  setup context do
+    server_type = Enum.random(context.server_types)
+    {:ok, server_type: server_type.server_type}
   end
 
-  test "create/2", %{payload: payload} do
-    assert {:ok, _} = ServerController.create(payload)
+  defp generate_params(server_type) do
+    %{server_type: server_type}
   end
 
-  describe "find/1" do
-    test "success", %{payload: payload} do
-      {:ok, serv} = ServerController.create(payload)
-      assert {:ok, serv} == ServerController.find(serv.server_id)
+  describe "server creating" do
+    test "creates server of given type", context do
+      params = generate_params(context.server_type)
+      {:ok, server} = ServerController.create(params)
+      {:ok, found_server} = ServerController.find(server.server_id)
+
+      # created server includes given id
+      assert params.server_type == server.server_type
+
+      # find yields the same previously created server
+      assert server.server_type == found_server.server_type
     end
 
-    test "failure" do
-      assert {:error, :notfound} == ServerController.find(PK.generate([]))
-    end
-  end
+    test "fails when server_type is invalid" do
+      params = %{server_type: Burette.Color.name()}
 
-  describe "update/2" do
-    test "change server location", %{payload: payload} do
-      assert {:ok, server} = ServerController.create(payload)
+      # assert that creating an server with invalid type raises Ecto.ConstraintError
+      assert_raise(Ecto.ConstraintError, fn ->
+        ServerController.create(params)
+      end)
 
-      poi = PK.generate([])
-      payload2 = %{poi_id: poi}
-      assert {:ok, server} = ServerController.update(server.server_id, payload2)
-      assert poi == to_string(server.poi_id)
-    end
-
-    test "change motherboard id", %{payload: payload} do
-      assert {:ok, server} = ServerController.create(payload)
-
-      mobo = PK.generate([])
-      payload2 = %{motherboard_id: mobo}
-      assert {:ok, server} = ServerController.update(server.server_id, payload2)
-      assert mobo == to_string(server.motherboard_id)
-    end
-
-    test "server not found" do
-      assert {:error, :notfound} == ServerController.update(PK.generate([]), %{})
+      # no server was created
+      refute Repo.get_by(Server, server_type: params.server_type)
     end
   end
 
-  test "delete/1 idempotency", %{payload: payload} do
-    {:ok, serv} = ServerController.create(payload)
-    assert :ok == ServerController.delete(serv.server_id)
-    assert :ok == ServerController.delete(serv.server_id)
-    assert {:error, :notfound} == ServerController.find(serv.server_id)
+  describe "server fetching" do
+    test "fetches existing server", context do
+      params = generate_params(context.server_type)
+      {:ok, server} = ServerController.create(params)
+
+      # an server is found
+      assert {:ok, found_server} = ServerController.find(server.server_id)
+
+      # the server is identical to the created one
+      assert server.server_id == found_server.server_id
+    end
+
+    test "fails to fetch when server is not found" do
+      assert {:error, :notfound} == ServerController.find(Random.pk())
+    end
+  end
+
+  test "delete is idempotent", context do
+    params = generate_params(context.server_type)
+    {:ok, server} = ServerController.create(params)
+
+    # server exists before deleting
+    assert Repo.get_by(Server, server_id: server.server_id)
+
+    :ok = ServerController.delete(server.server_id)
+    :ok = ServerController.delete(server.server_id)
+
+    # server was deleted
+    refute Repo.get_by(Server, server_id: server.server_id)
   end
 end
