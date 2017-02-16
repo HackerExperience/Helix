ca6742fd8428fd2929e7a5d7d06d55ec1493fa24
kress95 <cferraz95@gmail.com>
2017-02-16 18:34:34 -0200

    Add Entity factory and allow its use on tests.

---
+++ apps/entity/test/support/factory.ex
@@ -0,0 +1,50 @@
+defmodule Helix.Entity.Factory do
+
+  use ExMachina.Ecto, repo: Helix.Entity.Repo
+
+  alias HELL.PK
+  alias HELL.TestHelper.Random
+  alias Helix.Entity.Model.Entity
+  alias Helix.Entity.Model.EntityComponent
+  alias Helix.Entity.Model.EntityServer
+
+  def params(:entity) do
+    entity_type = generate_entity_type()
+
+    %{
+      entity_id: generate_pk(entity_type),
+      entity_type: entity_type
+    }
+  end
+
+  def entity_factory do
+    entity_type = generate_entity_type()
+
+    %Entity{
+      entity_id: generate_pk(entity_type),
+      entity_type: entity_type
+    }
+  end
+
+  def entity_component_factory do
+    %EntityComponent{
+      entity: build(:entity),
+      component_id: Random.pk()
+    }
+  end
+
+  def entity_server_factory do
+    %EntityServer{
+      entity: build(:entity),
+      server_id: Random.pk()
+    }
+  end
+
+  def generate_entity_type,
+    do: Enum.random(["account", "clan", "npc"])
+
+  def generate_pk("account"),
+    do: PK.generate([0x0000, 0x0000, 0x0000])
+  def generate_pk(_),
+    do: Random.pk()
+end
