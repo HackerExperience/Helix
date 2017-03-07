b9c3aae2d86e72c8154826e9efa0d47a3ceb4e1c
kress95 <cferraz95@gmail.com>
2017-03-07 15:52:14 -0300

    Remove useless code from Account factories.

--- apps/account/test/support/factory.ex
+++ apps/account/test/support/factory.ex
@@ -4,47 +4,9 @@ defmodule Helix.Account.Factory do
 
   alias HELL.PK
   alias HELL.TestHelper.Random
-  alias Helix.Account.Controller.AccountSetting, as: AccountSettingController
   alias Helix.Account.Model.Account
   alias Helix.Account.Model.AccountSetting
   alias Helix.Account.Model.Setting
-  alias Helix.Account.Repo
-
-  import Ecto.Query, only: [select: 3]
-
-  def params(:setting) do
-    %{
-      setting_id: setting_id(),
-      default_value: setting_value()
-    }
-  end
-
-  def params(:account_setting) do
-    account = insert(:account)
-    setting = insert(:setting)
-
-    %{
-      account: account,
-      account_id: account.account_id,
-      setting_id: setting.setting_id,
-      setting_value: setting_value()
-    }
-  end
-
-  def settings_for(account) do
-    custom_settings =
-      Setting
-      |> select([s], s.setting_id)
-      |> Repo.all()
-      |> Enum.map(&({&1, Random.string()}))
-      |> :maps.from_list()
-
-    Enum.each(custom_settings, fn {k, v} ->
-      AccountSettingController.put(account, k, v)
-    end)
-
-    custom_settings
-  end
 
   def account_factory do
     pk = PK.generate([0x0000, 0x0000, 0x0000])
