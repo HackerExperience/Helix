81a66087af448876d4f1427e509126d54e1434c4
kress95 <cferraz95@gmail.com>
2017-03-10 16:03:53 -0300

    Update MotherboardSlot query `from_motherboard` to accept both motherboard struct and pk.

--- apps/helix_hardware/lib/hardware/model/motherboard_slot.ex
+++ apps/helix_hardware/lib/hardware/model/motherboard_slot.ex
@@ -87,19 +87,17 @@ defmodule Helix.Hardware.Model.MotherboardSlot do
 
     import Ecto.Query, only: [select: 3, where: 3]
 
-    @spec from_motherboard(Ecto.Queryable.t, Motherboard.t) :: Ecto.Queryable.t
-    def from_motherboard(query \\ MotherboardSlot, m = %Motherboard{}) do
-      by_motherboard_id(query, m.motherboard_id)
-    end
+    @spec from_motherboard(Ecto.Queryable.t, Motherboard.t | Motherboard.id) ::
+      Ecto.Queryable.t
+    def from_motherboard(query \\ MotherboardSlot, motherboard_or_id)
+    def from_motherboard(query, motherboard = %Motherboard{}),
+      do: from_motherboard(query, motherboard.motherboard_id)
+    def from_motherboard(query, motherboard_id),
+      do: where(query, [ms], ms.motherboard_id == ^motherboard_id)
 
     @spec by_id(Ecto.Queryable.t, HELL.PK.t) :: Ecto.Queryable.t
     def by_id(query \\ MotherboardSlot, slot_id) do
       where(query, [ms], ms.slot_id == ^slot_id)
-    end
-
-    @spec by_motherboard_id(Ecto.Queryable.t, HELL.PK.t) :: Ecto.Queryable.t
-    def by_motherboard_id(query \\ MotherboardSlot, motherboard_id) do
-      where(query, [ms], ms.motherboard_id == ^motherboard_id)
     end
 
     @spec by_component_id(Ecto.Queryable.t, HELL.PK.t) :: Ecto.Queryable.t
