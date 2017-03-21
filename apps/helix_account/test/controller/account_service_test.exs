a4a0492373b8d845771c398ecc6af337b2147a3b
mememori <charlotte.elixir@gmail.com>
2017-03-21 10:03:27 -0300

    Ensure AccountService uses username for login

--- apps/helix_account/test/controller/account_service_test.exs
+++ apps/helix_account/test/controller/account_service_test.exs
@@ -3,6 +3,8 @@ defmodule Helix.Account.Controller.AccountServiceTest do
   use ExUnit.Case, async: true
 
   alias HELF.Broker
+  alias Helix.Account.Model.Account
+  alias Helix.Account.Repo
 
   alias Helix.Account.Factory
 
@@ -14,6 +16,38 @@ defmodule Helix.Account.Controller.AccountServiceTest do
       {_, {:ok, account}} = Broker.call("account.create", params)
 
       assert params.email === account.email
+    end
+  end
+
+  describe "login" do
+    test "uses username and password" do
+      password = Burette.Internet.password()
+      account =
+        Factory.insert(:account)
+        |> Account.update_changeset(%{password: password})
+        |> Repo.update!()
+
+      params = %{username: account.username, password: password}
+
+      assert {_, {:ok, _}} = Broker.call("account.login", params)
+    end
+
+    test "fails with any invalid combination" do
+      password = Burette.Internet.password()
+      account =
+        Factory.insert(:account)
+        |> Account.update_changeset(%{password: password})
+        |> Repo.update!()
+
+      permutations = [
+        %{username: account.username, password: "invalid_password"},
+        %{username: account.email, password: password},
+        %{username: account.account_id, password: password}
+      ]
+
+      Enum.each(permutations, fn params ->
+        assert {_, {:error, _}} = Broker.call("account.login", params)
+      end)
     end
   end
 end
