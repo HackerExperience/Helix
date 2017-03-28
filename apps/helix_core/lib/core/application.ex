82452831f47a682b02650ac1d9fea9e0e7d4f9c0
mememori <charlotte.elixir@gmail.com>
2017-03-27 23:52:47 -0300

    Merge helf_broker and helf_router onto helix_core as applications; update HELF dependency; update timex dependency. Closes #48. Closes #45

---
+++ apps/helix_core/lib/core/application.ex
@@ -0,0 +1,17 @@
+defmodule Helix.Core.Application do
+  use Application
+
+  @port Application.get_env(:helix_core, :router_port)
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(HELF.Router, [@port]),
+      worker(HELF.Broker, [])
+    ]
+
+    opts = [strategy: :one_for_one, name: Helix.Core.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
