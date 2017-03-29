760763135f9f121f608170b3cc053fa812a4116b
kress95 <cferraz95@gmail.com>
2017-03-29 18:02:28 -0300

    Update Process fetch/find API.

--- apps/helix_process/lib/process/controller/process.ex
+++ apps/helix_process/lib/process/controller/process.ex
@@ -1,29 +1,62 @@
 defmodule Helix.Process.Controller.Process do
 
-  import Ecto.Query, only: [where: 3]
-
+  alias HELL.PK
   alias Helix.Process.Repo
-  alias Helix.Process.Model.Process, as: ProcessModel
+  alias Helix.Process.Model.Process
+  alias Helix.Process.Model.Process.State
+
+  @type find_param ::
+    {:gateway, server :: PK.t}
+    | {:target, server :: PK.t}
+    | {:file, PK.t}
+    | {:network, PK.t}
+    | {:type, [String.t] | String.t}
+    | {:state, [State.state] | State.state}
 
   def create(process) do
-    ProcessModel.create_changeset(process)
+    process
+    |> Process.create_changeset()
     |> Repo.insert()
   end
 
-  def find(process_id) do
-    case Repo.get(ProcessModel, process_id) do
-      nil -> {:error, :notfound}
-      process -> {:ok, process}
-    end
+  @spec fetch(PK.t) :: Process.t | nil
+  def fetch(process_id),
+    do: Repo.get(Process, process_id)
+
+  @spec find([find_param], meta :: []) :: [Process.t]
+  def find(params, _meta \\ []) do
+    params
+    |> Enum.reduce(Process, &reduce_find_params/2)
+    |> Repo.all()
   end
 
-  def delete(process = %ProcessModel{}),
+  @spec delete(Process.t | PK.t) :: no_return
+  def delete(process = %Process{}),
     do: delete(process.process_id)
   def delete(process_id) do
-    ProcessModel
-    |> where([s], s.process_id == ^process_id)
+    process_id
+    |> Process.Query.by_id()
     |> Repo.delete_all()
 
     :ok
   end
+
+  @spec reduce_find_params(find_param, Ecto.Queryable.t) :: Ecto.Queryable.t
+  defp reduce_find_params({:gateway, server_id}, query),
+    do: Process.Query.by_gateway(query, server_id)
+  defp reduce_find_params({:target, server_id}, query),
+    do: Process.Query.by_target(query, server_id)
+  defp reduce_find_params({:file, file_id}, query),
+    do: Process.Query.by_file(query, file_id)
+  defp reduce_find_params({:network, network_id}, query),
+    do: Process.Query.by_network(query, network_id)
+  defp reduce_find_params({:type, type_list}, query) when is_list(type_list),
+    do: Process.Query.from_type_list(query, type_list)
+  defp reduce_find_params({:type, process_type}, query),
+    do: Process.Query.by_type(query, process_type)
+  defp reduce_find_params({:state, state_list}, query)
+  when is_list(state_list),
+    do: Process.Query.from_state_list(query, state_list)
+  defp reduce_find_params({:state, process_state}, query),
+    do: Process.Query.by_state(query, process_state)
 end
