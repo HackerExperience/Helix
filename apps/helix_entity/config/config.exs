6bc173308d3a79e0dffb31691418e27ff4594a52
mememori <charlotte.elixir@gmail.com>
2017-03-29 16:50:35 -0300

    Implement LTree. Closes #67. Closes #2

--- apps/helix_entity/config/config.exs
+++ apps/helix_entity/config/config.exs
@@ -8,6 +8,7 @@ config :helix_entity, Helix.Entity.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("HELIX_DB_USER") || "postgres",
   password: System.get_env("HELIX_DB_PASS") || "postgres",
-  hostname: System.get_env("HELIX_DB_HOST") || "localhost"
+  hostname: System.get_env("HELIX_DB_HOST") || "localhost",
+  types: HELL.PostgrexTypes
 
 import_config "#{Mix.env}.exs"
