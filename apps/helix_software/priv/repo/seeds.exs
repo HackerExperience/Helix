a48000fd77e3ced2e76d784972c8891c6a811bb3
mememori <charlotte.elixir@gmail.com>
2017-03-29 18:06:23 -0300

    Simplify File constants

--- apps/helix_software/priv/repo/seeds.exs
+++ apps/helix_software/priv/repo/seeds.exs
@@ -2,74 +2,20 @@ alias Helix.Software.Repo
 alias Helix.Software.Model.SoftwareType
 alias Helix.Software.Model.SoftwareModule
 
-types = [
-  %{
-    software_type: "cracker",
-    extension: "crc",
-    modules: ["password"]
-  },
-  %{
-    software_type: "exploit",
-    extension: "exp",
-    modules: ["ftp", "ssh"]
-  },
-  %{
-    software_type: "firewall",
-    extension: "fwl",
-    modules: ["active", "passive"]
-  },
-  %{
-    software_type: "hasher",
-    extension: "hash",
-    modules: ["password"]
-  },
-  %{
-    software_type: "log_forger",
-    extension: "logf",
-    modules: ["create", "edit"]
-  },
-  %{
-    software_type: "log_recover",
-    extension: "logr",
-    modules: ["recover"]
-  },
-  %{
-    software_type: "encryptor",
-    extension: "enc",
-    modules: ["file", "log", "connection", "process"]
-  },
-  %{
-    software_type: "decryptor",
-    extension: "dec",
-    modules: ["file", "log", "connection", "process"]
-  },
-  %{
-    software_type: "anymap",
-    extension: "map",
-    modules: ["geo", "inbound", "outbound"]
-  }
-]
-
 Repo.transaction fn ->
-  Enum.each(types, fn type ->
-    type
-    |> Map.take([:software_type, :extension])
-    |> SoftwareType.create_changeset()
-    |> Repo.insert!(on_conflict: :nothing)
+  Enum.each(SoftwareType.possible_types, fn {type, %{extension: extension}} ->
+    software_type = %SoftwareType{software_type: type, extension: extension}
+
+    Repo.insert!(software_type, on_conflict: :nothing)
   end)
 
-  modules = Enum.flat_map(types, fn type ->
-    Enum.map(type.modules, fn module_name ->
-      %{
-        software_type: type.software_type,
-        software_module: type.software_type <> "_" <> module_name
+  Enum.each(SoftwareType.possible_types, fn {type, %{modules: modules}} ->
+    Enum.each(modules, fn module ->
+      software_module = %SoftwareModule{
+        software_type: type,
+        software_module: module
       }
+      Repo.insert!(software_module, on_conflict: :nothing)
     end)
-  end)
-
-  Enum.each(modules, fn module ->
-    module
-    |> SoftwareModule.create_changeset()
-    |> Repo.insert!()
   end)
 end
