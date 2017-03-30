ea7cb7faaf5e5170b2a06c39c62590ab673f09f9
mememori <charlotte.elixir@gmail.com>
2017-03-29 23:17:29 -0300

    Fix Account router topics not working by implementing a barebones WS controller with a - barebones too - WS view for account and fix issues regarding to registering topic/broker for account actions on public api

---
+++ apps/helix_account/lib/ws/controller/account.ex
@@ -0,0 +1,42 @@
+defmodule Helix.Account.WS.Controller.Account do
+
+  alias Helix.Account.Service.API.Account, as: AccountAPI
+
+  @typep json_response ::
+    {:ok, map}
+    | {:error, status :: integer, map}
+    | {:error, atom}
+
+  @spec register(term, %{email: String.t, username: String.t, password: String.t, optional(atom) => any}) ::
+    json_response
+  # Plug-like format
+  def register(_request, params) do
+    case AccountAPI.create(params) do
+      {:ok, account} ->
+        # In the future we'll just pass the return to a protocol
+
+        # Note that we are wrapping it in a 2-tuple because Router expects it
+        # that way
+        {:ok, Helix.Account.WS.View.Account.format(account)}
+      {:error, changeset} ->
+        # Note that we are wrapping it in a 2-tuple because Router expects it
+        # that way
+        {:error, {400, Helix.Account.WS.View.Account.format(changeset)}}
+    end
+  end
+
+  @spec login(term, %{username: String.t, password: String.t, optional(atom) => any}) ::
+    json_response
+  def login(_request, %{username: username, password: password}) do
+    case AccountAPI.login(username, password) do
+      {:ok, account} ->
+        {:ok, Helix.Account.WS.View.Account.format(account)}
+      {:error, :notfound} ->
+        {:error, :notfound}
+    end
+  end
+
+  def login(_, _) do
+    {:error, :bad_request}
+  end
+end
--- apps/helix_account/lib/ws/routes.ex
+++ apps/helix_account/lib/ws/routes.ex
@@ -1,6 +1,7 @@
 defmodule Helix.Account.WS.Routes do
 
   alias HELF.Router
+  alias Helix.Account.WS.Controller.Account, as: AccountController
 
   @routes %{
     "account.create" => %{
@@ -9,7 +10,7 @@ defmodule Helix.Account.WS.Routes do
     },
     "account.login" => %{
       broker: "account.login",
-      atoms: ~w/username password/
+      atoms: ~w/username password/a
     }
   }
 
@@ -17,5 +18,20 @@ defmodule Helix.Account.WS.Routes do
     Enum.each(@routes, fn {topic, params} ->
       Router.register(topic, params.broker, params.atoms)
     end)
+  end
+
+  # This is a hack and only makes sense until we update Router to use Plug or
+  # Phoenix.Channels
+  @doc false
+  def register_topics do
+    account_create_fun = fn _pid, _topic, message, _request ->
+      {:reply, AccountController.register(nil, message)}
+    end
+    HELF.Broker.subscribe("account.create", call: account_create_fun)
+
+    account_login_fun = fn _pid, _topic, message, _request ->
+      {:reply, AccountController.login(nil, message)}
+    end
+    HELF.Broker.subscribe("account.login", call: account_login_fun)
   end
 end
