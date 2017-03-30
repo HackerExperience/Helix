d294b1df71511f5ad8f37a2f014992f3bed35436
kress95 <cferraz95@gmail.com>
2017-03-30 17:24:32 -0300

    Drop HardwareService.

--- apps/helix_hardware/lib/hardware.ex
+++ apps/helix_hardware/lib/hardware.ex
@@ -2,15 +2,13 @@ defmodule Helix.Hardware.App do
 
   use Application
 
-  alias Helix.Hardware.Controller.HardwareService
   alias Helix.Hardware.Repo
 
   def start(_type, _args) do
     import Supervisor.Spec
 
     children = [
-      worker(Repo, []),
-      worker(HardwareService, [])
+      worker(Repo, [])
     ]
 
     opts = [strategy: :one_for_one, name: Helix.Hardware.Supervisor]
