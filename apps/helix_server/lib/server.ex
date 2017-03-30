6e05f33e801a894c794230986163d3bf115c5d2e
kress95 <cferraz95@gmail.com>
2017-03-30 13:44:33 -0300

    Remove deprecated ServerService.

--- apps/helix_server/lib/server.ex
+++ apps/helix_server/lib/server.ex
@@ -2,15 +2,13 @@ defmodule Helix.Server.App do
 
   use Application
 
-  alias Helix.Server.Controller.ServerService
   alias Helix.Server.Repo
 
   def start(_type, _args) do
     import Supervisor.Spec
 
     children = [
-      worker(Repo, []),
-      worker(ServerService, [])
+      worker(Repo, [])
     ]
 
     opts = [strategy: :one_for_one, name: Helix.Server.Supervisor]
