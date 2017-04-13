439ae6feb816dcac6a43c027ba66e6b7fb9e8771
Renato Massaro <renato@hackerexperience.com>
2017-04-13 07:54:06 -0300

    Support CORS

--- config/prod.exs
+++ config/prod.exs
@@ -6,6 +6,9 @@ config :logger,
   metadata: [:request_id]
 
 config :helix, Helix.Endpoint,
+  allowed_cors: ["https://api.hackerexperience.com",
+                 "https://1.hackerexperience.com",
+                 "https://heborn.hackerexperience.com"],
   server: true,
   url: [host: "${HELIX_ENDPOINT_URL}", port: 80],
   secret_key_base: "${HELIX_ENDPOINT_SECRET_KEY}",
