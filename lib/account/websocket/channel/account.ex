1995629b05d71ea58285335cbf967a1da05689cd
mememori <charlotte.elixir@gmail.com>
2017-05-12 15:14:39 -0300

    Add request-event for servers owned by account on WS

--- lib/account/websocket/channel/account.ex
+++ lib/account/websocket/channel/account.ex
@@ -5,6 +5,10 @@ defmodule Helix.Account.Websocket.Channel.Account do
 
   use Phoenix.Channel
 
+  alias Helix.Hardware.Controller.Motherboard, as: MotherboardController
+  alias Helix.Hardware.Service.API.Component, as: ComponentAPI
+  alias Helix.Hardware.Service.API.Motherboard, as: MotherboardAPI
+  alias Helix.Server.Service.API.Server, as: ServerAPI
   alias Helix.Entity.Service.API.HackDatabase
   alias Helix.Entity.Service.API.Entity
 
@@ -27,6 +31,72 @@ defmodule Helix.Account.Websocket.Channel.Account do
       |> HackDatabase.get_database()
 
     {:reply, {:ok, %{data: %{entries: hack_database}}}, socket}
+  end
+
+  # TODO: Fetch server's IPs
+  def handle_in("server.index", _message, socket) do
+    servers =
+      socket.assigns.account
+      |> Entity.get_entity_id()
+      |> Entity.fetch()
+      |> Entity.get_servers_from_entity()
+      |> Enum.map(&ServerAPI.fetch/1)
+      |> Enum.map(fn
+        server = %{motherboard_id: motherboard} when not is_nil(motherboard) ->
+          motherboard =
+            server.motherboard_id
+            |> ComponentAPI.fetch()
+            |> MotherboardAPI.fetch!()
+            |> MotherboardAPI.preload_components()
+
+          {server, motherboard}
+        server ->
+          {server, nil}
+      end)
+      |> Enum.map(&render_server/1)
+
+    {:reply, {:ok, %{data: %{servers: servers}}}, socket}
+  end
+
+  # TODO: Move this to a viewer
+  def render_server({server, nil}) do
+    %{
+      server_id: server.server_id,
+      server_type: server.server_type,
+      password: server.password,
+      hardware: nil,
+      ips: []
+    }
+  end
+  def render_server({server, motherboard}) do
+    %{
+      server_id: server.server_id,
+      server_type: server.server_type,
+      password: server.password,
+      hardware: %{
+        # FIXME: This is querying the db again for the components
+        resources: MotherboardController.resources(motherboard),
+        components: render_components(motherboard)
+      },
+      ips: []
+    }
+  end
+
+  defp render_components(%{slots: slots}) do
+    slots
+    |> Enum.map(fn slot = %{component: component = %{}} ->
+      internal_id = slot.slot_internal_id
+
+      data = %{
+        component_id: component.component_id,
+        component_type: component.component_type,
+        # TODO: Return data about component specialization
+        meta: %{}
+      }
+
+      {internal_id, data}
+    end)
+    |> :maps.from_list()
   end
 
   def notify(account_id, notification) do
--- test/account/websocket/channel/account_test.exs
+++ test/account/websocket/channel/account_test.exs
@@ -3,9 +3,11 @@ defmodule Helix.Account.Websocket.Channel.AccountTest do
   use Helix.Test.IntegrationCase
 
   alias Helix.Websocket.Socket
+  alias Helix.Server.Model.ServerType
   alias Helix.Account.Service.API.Session
   alias Helix.Account.Websocket.Channel.Account, as: Channel
 
+  alias Helix.Entity.Factory, as: EntityFactory
   alias Helix.Account.Factory
 
   import Phoenix.ChannelTest
@@ -21,10 +23,91 @@ defmodule Helix.Account.Websocket.Channel.AccountTest do
     {:ok, account: account, socket: socket}
   end
 
+  defp create_server_for_entity(entity) do
+    alias Helix.Hardware.Factory, as: HardwareFactory
+    alias Helix.Hardware.Service.API.Motherboard, as: MotherboardAPI
+    alias Helix.Entity.Service.API.Entity, as: EntityAPI
+    alias Helix.Server.Service.API.Server, as: ServerAPI
+    alias Helix.Server.Factory, as: ServerFactory
+
+    # FIXME PLEASE
+    server = ServerFactory.insert(:server)
+    EntityAPI.link_server(entity, server.server_id)
+
+    # I BEG YOU, SAVE ME FROM THIS EXCRUCIATING PAIN
+    motherboard = HardwareFactory.insert(:motherboard)
+    Enum.each(motherboard.slots, fn slot ->
+      component = HardwareFactory.insert(slot.link_component_type)
+      component = component.component
+
+      MotherboardAPI.link(slot, component)
+    end)
+    {:ok, server} = ServerAPI.attach(server, motherboard.motherboard_id)
+
+    server
+  end
+
   test "an user can't join another user's notification channel", context do
     another_account = Factory.insert(:account)
     id = another_account.account_id
     assert {:error, _} = join(context.socket, "account:" <> id)
+  end
+
+  describe "server.index" do
+    test "returns all servers owned by the account", context do
+      entity = EntityFactory.insert(
+        :entity,
+        entity_id: context.account.account_id)
+
+      server_ids = Enum.map(1..5, fn _ ->
+        server = create_server_for_entity(entity)
+
+        server.server_id
+      end)
+
+      ref = push context.socket, "server.index", %{}
+
+      assert_reply ref, :ok, response
+
+      received_server_ids =
+        response.data.servers
+        |> Enum.map(&(&1.server_id))
+        |> MapSet.new()
+
+      # TODO: improve those format checks
+      assert MapSet.equal?(MapSet.new(server_ids), received_server_ids)
+      assert Enum.all?(response.data.servers, fn server ->
+        match?(
+          %{
+            server_id: _,
+            server_type: _,
+            password: _,
+            hardware: _,
+            ips: _
+          },
+          server)
+      end)
+      assert Enum.all?(response.data.servers, &(is_binary(&1.server_id)))
+      assert Enum.all?(response.data.servers, fn server ->
+        server.server_type in ServerType.possible_types()
+      end)
+      assert Enum.all?(response.data.servers, &(is_binary(&1.password)))
+      assert Enum.all?(response.data.servers, fn
+        %{hardware: nil} ->
+          true
+        %{hardware: hardware = %{}} ->
+          match?(
+            %{
+              resources: %{
+                cpu: _,
+                ram: _,
+                net: %{}
+              },
+              components: %{}
+            },
+            hardware)
+      end)
+    end
   end
 
   describe "notification/2" do
