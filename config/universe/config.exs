b596d5684022dd629f6c45d1f69109fe81d3af98
Renato Massaro <renato@hackerexperience.com>
2017-08-09 09:06:34 -0300

    Remove old NPC service

--- config/universe/config.exs
+++ config/universe/config.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :helix, Helix.Universe.Repo,
   priv: "priv/repo/universe",
-  size: 4,
+  pool_size: 3,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("HELIX_DB_USER") || "postgres",
   password: System.get_env("HELIX_DB_PASS") || "postgres",
