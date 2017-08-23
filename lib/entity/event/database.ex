83af458a306f2e6d5fa02fc63211e59d76d92ffb
Renato Massaro <renato@hackerexperience.com>
2017-08-23 03:35:33 -0300

    Temporarly disable CrackerConclusionEvent handler

--- lib/entity/event/database.ex
+++ lib/entity/event/database.ex
@@ -1,11 +1,7 @@
 defmodule Helix.Entity.Event.Database do
 
-  alias Helix.Server.Query.Server, as: ServerQuery
   alias Helix.Universe.Bank.Query.Bank, as: BankQuery
   alias Helix.Entity.Action.Database, as: DatabaseAction
-  alias Helix.Entity.Query.Database, as: DatabaseQuery
-  alias Helix.Entity.Query.Entity, as: EntityQuery
-  alias Helix.Entity.Repo
 
   alias Helix.Software.Model.SoftwareType.Cracker.ProcessConclusionEvent
   alias Helix.Universe.Bank.Model.BankAccount.PasswordRevealedEvent,
@@ -14,33 +10,33 @@ defmodule Helix.Entity.Event.Database do
     as: BankAccountLoginEvent
   alias Helix.Universe.Bank.Model.BankTokenAcquiredEvent
 
-  def cracker_conclusion(event = %ProcessConclusionEvent{}) do
-    entity = EntityQuery.fetch(event.entity_id)
-    server = ServerQuery.fetch(event.server_id)
-    server_ip = ServerQuery.get_ip(event.server_id, event.network_id)
+  def cracker_conclusion(_event = %ProcessConclusionEvent{}) do
+    # entity = EntityQuery.fetch(event.entity_id)
+    # server = ServerQuery.fetch(event.server_id)
+    # server_ip = ServerQuery.get_ip(event.server_id, event.network_id)
 
-    create_entry = fn ->
-      DatabaseAction.add_server(
-        entity,
-        event.network_id,
-        event.server_ip,
-        server,
-        event.server_type)
-    end
+    # create_entry = fn ->
+    #   DatabaseAction.add_server(
+    #     entity,
+    #     event.network_id,
+    #     event.server_ip,
+    #     server,
+    #     event.server_type)
+    # end
 
-    # Review: Why is it updating everyone's password?
-    set_password = fn ->
-      entity
-      |> DatabaseQuery.get_server_entries(server)
-      |> Enum.each(&DatabaseAction.update(&1, %{password: server.password}))
-    end
+    # # Review: Why is it updating everyone's password?
+    # set_password = fn ->
+    #   entity
+    #   |> DatabaseQuery.get_server_entries(server)
+    #   |> Enum.each(&DatabaseAction.update(&1, %{password: server.password}))
+    # end
 
-    if to_string(server_ip) == to_string(event.server_ip) do
-      Repo.transaction fn ->
-        {:ok, _} = create_entry.()
-        :ok = set_password.()
-      end
-    end
+    # if to_string(server_ip) == to_string(event.server_ip) do
+    #   Repo.transaction fn ->
+    #     {:ok, _} = create_entry.()
+    #     :ok = set_password.()
+    #   end
+    # end
   end
 
   @doc """
