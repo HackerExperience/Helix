2894d31764f21e93c7454ccc9e17bd6a9a7688b6
Renato Massaro <renato@hackerexperience.com>
2017-08-27 22:41:14 -0300

    Move dialyzer-protocol hack to a specific macro

---
+++ lib/hell/hack.ex
@@ -0,0 +1,90 @@
+###########################################
+# IGNORE THE FOLLOWING LINES.
+# Dialyzer is not particularly a fan of protocols, so it will emit a lot of
+# "unknown functions" for non-implemented types on a protocol. This hack will
+# implement any possible type to avoid those warnings (albeit it might increase
+# the compilation time in a second)
+###########################################
+
+defmodule HELL.Hack.Experience do
+
+  defmacro protocolols do
+
+    protocols = [
+      Helix.Cache.Model.Cacheable,
+      Helix.Event.Notificable,
+      Helix.Websocket.Requestable,
+      Helix.Process.Model.Process.ProcessType,
+      Helix.Process.API.View.Process
+    ]
+
+    methods = %{
+      "Elixir.Helix.Cache.Model.Cacheable" => [{:format_output, 1}],
+      "Elixir.Helix.Event.Notificable" => [
+        {:whom_to_notify, 1},
+        {:generate_payload, 2}
+      ],
+      "Elixir.Helix.Websocket.Requestable" => [
+        {:check_params, 2},
+        {:check_permissions, 2},
+        {:handle_request, 2},
+        {:reply, 2}
+      ],
+      "Elixir.Helix.Process.Model.Process.ProcessType" => [
+        {:dynamic_resources, 1},
+        {:state_change, 4},
+        {:kill, 3},
+        {:minimum, 1},
+        {:conclusion, 2}
+      ],
+      "Elixir.Helix.Process.API.View.Process" => [
+        {:render, 4}
+      ]
+    }
+
+    impls = [
+      Atom,
+      BitString,
+      Float,
+      Function,
+      Integer,
+      List,
+      Map,
+      PID,
+      Port,
+      Reference,
+      Tuple
+    ]
+
+    Enum.map(protocols, fn protocol ->
+      functions = methods[to_string(protocol)]
+
+      for impl <- impls do
+        quote do
+          defimpl unquote(protocol), for: unquote(impl) do
+
+            unquote (
+              Enum.map(functions, fn {name, arity} ->
+                args = List.duplicate(quote do _ end, arity)
+
+                quote do
+                  def unquote(name)(unquote_splicing(args)) do
+                    raise \
+                      "#{inspect unquote(protocol)} not implemented " <>
+                      "for #{inspect unquote(impl)}"
+                  end
+                end
+              end)
+            )
+          end
+        end
+      end
+    end)
+  end
+end
+
+defmodule HELL.Hack.Experience2 do
+  require HELL.Hack.Experience
+
+  HELL.Hack.Experience.protocolols()
+end
