daf8a1b8088d3c289554a259aebfe4d35eaa0c12
Renato Massaro <renato@hackerexperience.com>
2017-11-22 06:44:21 -0200

    Remove legacy Hardware service

--- config/hardware/test.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-prefix = System.get_env("HELIX_DB_PREFIX") || "helix"
-
-config :helix, Helix.Hardware.Repo,
-  pool: Ecto.Adapters.SQL.Sandbox,
-  database: prefix <> "_test_hardware",
-  ownership_timeout: 90_000
