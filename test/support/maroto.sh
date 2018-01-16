816e67b7452939311d6782de2c252090ab7614a4
Renato Massaro <renato@hackerexperience.com>
2018-01-16 03:20:29 -0200

    Add maroto.sh

---
+++ test/support/maroto.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# Go to Helix root
+cd $(dirname $(realpath $0))
+cd ../../
+
+HELIX_ROOT=$PWD
+MIX_FILE=$HELIX_ROOT/mix.exs
+IEX_FILE=$HELIX_ROOT/.iex.exs
+
+# Replace elixirc option; ensures `test/support` gets compiled on :dev
+ed -s $HELIX_ROOT/mix.exs <<< $',s/(:test)/(_)/g\nw'
+
+# Create temporary IEx file with instructions
+echo "IO.puts \"\n\nType 'use Helix.Maroto' for maximum marotagem!!11!\n\n\"" > $IEX_FILE
+
+# Run Helix interactively
+iex -S mix
+
+# Clean up any changes
+git checkout $MIX_FILE
+rm $IEX_FILE
