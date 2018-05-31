cce64311e45c59ec4955f360941459cfa4e8ff7f
Marcelo Amancio de Lima Santos <marcelo.a.lima.santos@gmail.com>
2018-05-31 00:19:05 -0300

    Improve logout request

--- lib/universe/bank/websocket/channel/requests/logout.ex
+++ lib/universe/bank/websocket/channel/requests/logout.ex
@@ -2,7 +2,12 @@ import Helix.Websocket.Request
 
 request Helix.Universe.Bank.Websocket.Requests.Logout do
 
+  alias Helix.Event
   alias Helix.Websocket
+
+  alias Helix.Universe.Bank.Event.Bank.Account.Logout,
+    as: BankAccountLogoutEvent
+  alias Helix.Universe.Bank.Query.Bank, as: BankQuery
 
   def check_params(request, _socket),
     do: reply_ok(request)
@@ -11,9 +16,20 @@ request Helix.Universe.Bank.Websocket.Requests.Logout do
     do: reply_ok(request)
 
   def handle_request(request, socket) do
+    atm_id = socket.assigns.atm_id
+    account_number = socket.assigns.account_number
+    account = BankQuery.fetch_account(atm_id, account_number)
+
+    entity_id = socket.assigns.gateway.entity_id
+
+    events =
+      [BankAccountLogoutEvent.new(account, entity_id)]
+
     socket
     |> Websocket.id()
     |> Helix.Endpoint.broadcast("disconnect", %{})
+
+    Event.emit(events)
 
     reply_ok(request)
   end
